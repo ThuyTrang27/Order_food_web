6ed52e4ac3f8f75c62250a95d4d2f7e58a7ce306
ThuyTrang27 <nguyenthuytrang2021bd@gmail.com>
2026-03-26 12:13:37 +0700

    first commit

---
+++ my-react-app/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react' // Bỏ useState vì dùng qua Context rồi
+import './FoodItem.css'
+import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext' // THIẾU DÒNG NÀY
+
+const FoodItem = ({ id, name, price, description, image }) => {
+
+    // Lấy dữ liệu và hàm từ StoreContext
+    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+
+    return (
+        <div className='food-item'>
+            <div className='food-item-img-container'>
+                <img className='food-item-img' src={image} alt={name} />
+                {!cartItems[id]
+                    ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
+                    : <div className='food-item-counter'>
+                        <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+                        <p>{cartItems[id]}</p>
+                        <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
+                    </div>
+                }
+            </div>
+            <div className="food-item-info">
+                <div className="food-item-name-rating">
+                    <p>{name}</p>
+                    <img src={assets.rating_starts} alt="rating" />
+                </div>
+                <p className="food-item-desc">{description}</p>
+                <p className="food-item-price">${price}</p>
+            </div>
+        </div>
+    )
+}
+
+export default FoodItem
